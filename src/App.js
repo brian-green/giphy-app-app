f71397021fb2e0fac0202d601c61da45361ed088
Brian Green <dear.briangreen@gmail.com>
2020-11-16 23:27:36 -0800

    created barebones react app
    initiated react-create-app, added page header.

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+const Header = () => (
+  <div className="header grid">
+      <h1 className="title">Jiffy</h1>
+  </div>
+)
+
+class App extends Component {
+  render () {
+    return (
+      <div className="page">
+        <Header/>
+      </div>
+    );
+  }
+}
+
+export default App;
