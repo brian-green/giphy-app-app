36765e935ed4a99a578809e71d0f70bd39414a57
Brian Green <dear.briangreen@gmail.com>
2020-11-17 23:59:27 -0800

    made search component and events

--- src/App.js
+++ src/App.js
@@ -6,16 +6,39 @@ const Header = () => (
   </div>
 )
 
+const handleChange = event => {
+  // same as…
+  // const value = event.target.value
+  const {value} = event.target
+  // event.key and event.keyCode are empty with onChange
+}
+
+const handleKeyPress = event => {
+  const {value} = event.target
+  // value gives us the previous updated value
+  // which is why we need to use onChange
+  if (event.key === 'Enter') {
+    alert('you pressed enter!')
+  }
+}
+
+const Search = () => (
+  <div className="search grid">
+    <input
+      className="input grid-item"
+      placeholder="Type something"
+      onChange={handleChange}
+      onKeyPress={handleKeyPress}
+    />
+  </div>
+)
+
 class App extends Component {
   render () {
     return (
       <div className="page">
         <Header/>
-        <div className='search grid'>
-          {/*Where the gif stack goes */}
-          <input className='input grid-item' placeholder='type something here'
-          onChange={this.handleChange}/>
-        </div>
+        <Search/>
       </div>
     );
   }
