d664f23d599bff2c6176a00ad75c1511c27dfbb5
Brian Green <dear.briangreen@gmail.com>
2020-11-18 23:13:39 -0800

    Added key press function

--- src/App.js
+++ src/App.js
@@ -7,18 +7,30 @@ const Header = () => (
 )
 
 const handleChange = event => {
+  //create react app can write methods as arrow functions
+  //meaning no constructor and bind
+  const {value} = event.target
   // same as…
   // const value = event.target.value
-  const {value} = event.target
+  
+  // value allows us to see the value of the typing, 
+  // target is element, value is the input
+
   // event.key and event.keyCode are empty with onChange
+  console.log(value);
+  if (value.length > 2) {
+    console.log('This is a valid search term.');
+  }
 }
 
+// where there are 2 or more characters and press enter
+// run a search
 const handleKeyPress = event => {
   const {value} = event.target
   // value gives us the previous updated value
   // which is why we need to use onChange
-  if (event.key === 'Enter') {
-    alert('you pressed enter!')
+  if (value.length > 2 && event.key === 'Enter') {
+    alert(`search for ${value}`)
   }
 }
 
@@ -27,6 +39,9 @@ const Search = () => (
     <input
       className="input grid-item"
       placeholder="Type something"
+      // here we are running functions every time it changes
+      // react convention, events are right on the component
+
       onChange={handleChange}
       onKeyPress={handleKeyPress}
     />
