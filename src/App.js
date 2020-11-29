86731a13e374fc76f1478d97bb638302ae4fb49b
Brian Green <dear.briangreen@gmail.com>
2020-11-28 20:25:10 -0800

    Adding Search Capabilities (blocker)
    This doesn't complie. Tracking issue. Apparent react create app has some new reserved keywords that make this syntax not work.
    
    Failed to compile.
    
    src/App.js
      Line 45:21:  'handleChange' is not defined    no-undef
      Line 46:23:  'handleKeyPress' is not defined  no-undef

--- src/App.js
+++ src/App.js
@@ -2,58 +2,51 @@ import React, {Component} from 'react';
 
 const Header = () => (
   <div className="header grid">
-      <h1 className="title">Jiffy Lube</h1>
+      <h1 className="title">Jiffy</h1>
   </div>
-)
+);
 
-const handleChange = event => {
-  //create react app can write methods as arrow functions
-  //meaning no constructor and bind
-  const {value} = event.target
-  // same as…
-  // const value = event.target.value
-  
-  // value allows us to see the value of the typing, 
-  // target is element, value is the input
+class App extends Component { 
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm: ''
+    };
 
-  // event.key and event.keyCode are empty with onChange
-  console.log(value);
-  if (value.length > 2) {
-    console.log('This is a valid search term.');
   }
-}
 
-// where there are 2 or more characters and press enter
-// run a search
-const handleKeyPress = event => {
-  const {value} = event.target
-  // value gives us the previous updated value
-  // which is why we need to use onChange
-  if (value.length > 2 && event.key === 'Enter') {
-    alert(`search for ${value}`)
+  handleChange = event => {
+    const {value} = event.target;
+    this.setState((prevState, props) => ({
+      ...prevState,
+      searchTerm: value
+    }));
+    if (value.length > 2) {
+    }
   }
-}
 
-const Search = () => (
-  <div className="search grid">
-    <input
-      className="input grid-item"
-      placeholder="Type something"
-      // here we are running functions every time it changes
-      // react convention, events are right on the component
+  handleKeyPress = event => {
+    const {value} = event.target;
+    if (value.length > 2 && event.key === 'Enter') {
+      alert(`search for ${value}`);
+    }
+  }
 
-      onChange={handleChange}
-      onKeyPress={handleKeyPress}
-    />
-  </div>
-)
 
-class App extends Component {
   render () {
+    const {searchTerm} = this.state;
     return (
       <div className="page">
         <Header/>
-        <Search/>
+        <div className="search grid">
+        <input
+          className="input grid-item"
+          placeholder="Type something"
+          onChange={handleChange}
+          onKeyPress={handleKeyPress}
+          value={searchTerm}
+        />
+        </div>
       </div>
     );
   }
